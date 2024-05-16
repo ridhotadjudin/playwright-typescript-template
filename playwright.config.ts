8ecd7dd24c865cdae5f31ea7c538251d3b5d18b0
ridhotadjudin <ridhotadjudin@gmail.com>
2024-05-17 00:23:18 +0700

    update<config>: configuration and arrangement of files

--- playwright.config.ts
+++ playwright.config.ts
@@ -11,45 +11,67 @@ import { defineConfig, devices } from '@playwright/test';
  */
 export default defineConfig({
   testDir: './tests',
+  
   /* Run tests in files in parallel */
   fullyParallel: true,
+  
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
+  
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  // retries: process.env.CI ? 2 : 0,
+  retries:2,
+
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
+  
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
+  // reporter: [['list'],['json', {outputFile: 'test-results.json'}]],
+  
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'http:staging-chief.nexcloud.id',
+    baseURL: 'http://127.0.0.1:3000',
+    // baseURL: 'http:staging-chief.nexcloud.id',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
-
     screenshot:'only-on-failure',
-
-    headless: false
+    headless: false,
+    // ignoreHTTPSErrors: true,
+    viewport: {width: 1280, height:720},
+    video: 'on-first-retry',
+    // timeout: 30000,
   },
+
+  /* Folder for test artifact such as screenshots, videos, traces, etc. */
+  // outputDir: 'test-results/',
 
   /* Configure projects for major browsers */
   projects: [
     {
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
-    }//,
+    },
 
-    // {
-    //   name: 'firefox',
-    //   use: { ...devices['Desktop Firefox'] },
-    // },
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
 
-    // {
-    //   name: 'webkit',
-    //   use: { ...devices['Desktop Safari'] },
-    // },
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] },
+    },
+
+    {
+      name: 'all-browsers-and-tests',
+      use: {
+        baseURL: 'https://playwright.dev',
+        use: { ...devices['Desktop Chrome'] }
+      }
+    },
 
     /* Test against mobile viewports. */
     // {
